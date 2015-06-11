8eb71e3d0f6671b8916e3fd82abedeff23a54f7a
Danny Wilson <danny@decube.net>
2015-06-11 17:14:00 +0200

    Allow building in Jenkins as maven project

--- project.clj
+++ project.clj
@@ -5,4 +5,17 @@
   :license {:name "MIT-style license (see LICENSE for details)."}
   :global-vars {*warn-on-reflection* true}
   :profiles {:test {:resource-paths ["test"]}}
+  :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.3.23"
+                 {:extensions "true"
+                  :executions ([:execution
+                                [:id "clojure-compile"]
+                                [:phase "compile"]
+                                [:configuration
+                                 [:temporaryOutputDirectory "true"]
+                                 [:sourceDirectories [:sourceDirectory "src"]]]
+                                [:goals [:goal "compile"]]]
+                                 [:execution
+                                  [:id "clojure-test"]
+                                  [:phase "test"]
+                                  [:goals [:goal "test"]]])}]]
   :dependencies [[org.clojure/clojure "1.5.1"]])
